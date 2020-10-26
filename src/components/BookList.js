e8c2ad70392d1579dcbce18161b86ae62d6dbf2f
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-10-25 23:05:06 -0400

    app returning a list of book titles from query

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 const BookList = () => {
-	const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState(null);
 	useEffect(() => {
 		fetch(
 			`https://www.googleapis.com/books/v1/volumes?q=intitle:harry+potter+and+the+sorcerers+stone&key=${process.env.REACT_APP_KEY}`
 		)
 			.then((res) => res.json())
 			.then((res) => {
-				setBooks(res);
+				setBooks(res.items);
 			})
 			.catch(console.error);
 	}, []);
-	console.log(books);
+	if (books === null) {
+		return null;
+	}
 	return (
 		<div>
-			<h1>Hello Book List</h1>
+			<ul>
+				{books.map((book) => {
+					return <li>{book.volumeInfo.title}</li>;
+				})}
+			</ul>
 		</div>
 	);
 };
