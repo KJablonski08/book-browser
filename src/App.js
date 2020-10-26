1b4e4e348b7566553d5c4ba72ca6321df60353e3
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-10-25 22:29:10 -0400

    added Home and Nav components with reat router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
 
 const App = () => {
-	return <div>Hello World</div>;
+	return (
+		<div>
+			<header>
+				<Nav />
+			</header>
+			<main>
+				<Route path='/' exact component={Home} />
+			</main>
+		</div>
+	);
 };
 
 export default App;
