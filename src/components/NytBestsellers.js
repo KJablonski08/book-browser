72f4b96b49a0788496415eb6b160de8142b9b698
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-11-01 14:19:53 -0500

    added nonfiction list to home

--- src/components/NytBestsellers.js
+++ src/components/NytBestsellers.js
@@ -3,7 +3,7 @@ import { Carousel, Container, Button } from 'react-bootstrap';
 
 const NytBestsellers = ({ searchObj, setSearchObj, history }) => {
 	const [list, setList] = useState(null);
-	const [bestSeller, setBestSeller] = useState(null);
+	const [nfList, setnfList] = useState(null);
 	useEffect(() => {
 		fetch(
 			`https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=${process.env.REACT_APP_NYT_KEY}`
@@ -13,13 +13,21 @@ const NytBestsellers = ({ searchObj, setSearchObj, history }) => {
 				setList(res);
 			})
 			.catch(console.error);
+		fetch(
+			`https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=${process.env.REACT_APP_NYT_KEY}`
+		)
+			.then((res) => res.json())
+			.then((res) => {
+				setnfList(res);
+			})
+			.catch(console.error);
 	}, []);
 	const handleClick = (bestseller) => {
 		setSearchObj({ ...searchObj, isbn: bestseller.primary_isbn10 });
 		history.push('/books');
 	};
 
-	if (!list) {
+	if (!list || !nfList) {
 		return (
 			<div className='book-list'>
 				<p>Book List not Found</p>
@@ -33,6 +41,25 @@ const NytBestsellers = ({ searchObj, setSearchObj, history }) => {
 				<h2>Best Sellers: {list.results.list_name}</h2>
 				<Carousel>
 					{list.results.books.map((bestseller, i) => {
+						return (
+							<Carousel.Item key={i} onClick={() => handleClick(bestseller)}>
+								<span>{bestseller.rank} </span>
+								<img
+									className='nyt-img'
+									src={bestseller.book_image}
+									alt='First slide'
+								/>
+							</Carousel.Item>
+						);
+					})}
+				</Carousel>
+			</Container>
+			<br />
+			<Container className='authors'>
+				<h4>The New York Times Best Sellers</h4>
+				<h2>Best Sellers: {nfList.results.list_name}</h2>
+				<Carousel>
+					{nfList.results.books.map((bestseller, i) => {
 						return (
 							<Carousel.Item key={i} onClick={() => handleClick(bestseller)}>
 								<span>{bestseller.rank} </span>
